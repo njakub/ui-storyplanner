2658db23038305a472316912ef40637095869c4d
njakub <njakub94@gmail.com>
2019-06-30 10:33:43 +0100

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,15 @@ import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import PageNotFound from "./PageNotFound";
 import CoursesPage from "./courses/CoursesPage";
-import style from "./App.scss";
 
 function App() {
   return (
-    <div className={style.app}>
-      <div className={style.sassyDiv}>Get Sassssssy!</div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/courses" component={CoursesPage} />
-        <Route component={PageNotFound} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/courses" component={CoursesPage} />
+      <Route component={PageNotFound} />
+    </Switch>
   );
 }
 
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AppShell from "../appShell/AppShell";
 
 const HomePage = () => (
-  <AppShell>
-    <div className="jumbotron">
-      <h1>Pluralsight Administration</h1>
-      <p>React, Redux and React Router for ultra-responsive web apps.</p>
-      <Link to="about" className="btn btn-primary btn-lg">
-        Learn more
-      </Link>
-    </div>
-  </AppShell>
+  <div className="jumbotron">
+    <h1>Pluralsight Administration</h1>
+    <p>React, Redux and React Router for ultra-responsive web apps.</p>
+    <Link to="about" className="btn btn-primary btn-lg">
+      Learn more
+    </Link>
+  </div>
 );
 
 export default HomePage;
